f146cef55dadbc4e1653d9034d39cc977b63fd7c
jeevjacobgeorge <jeev@J4Hp>
2024-02-05 08:14:54 +0530

    Old version

--- algorithm/al_urls.py
+++ algorithm/al_urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('',views.att_pred,name='attendance'),
-  path('attendance/',views.att_pred,name='attendance1'),
+  path('attendance/',views.att_pred,name='attendance'),
 
-]
+] 
